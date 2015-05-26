b88f99e530d8e5e1446ee6662e84e43d404d1aad
marciodscampos <marciodscampos@gmail.com>
2015-05-25 22:45:04 -0300

    Botão cancelar adicionado

--- src/gui/AddUserController.java
+++ src/gui/AddUserController.java
@@ -2,10 +2,14 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -15,11 +19,14 @@ import java.io.IOException;
 public class AddUserController {
     @FXML private SplitMenuButton userType;
     @FXML private Button okBtn;
+    @FXML private Button cancelBtn;
     @FXML private TextField firstNameField;
     @FXML private TextField lastNameField;
     @FXML private TextField userIdField;
 
-    @FXML public void handleButtonAction(ActionEvent event) throws IOException {
+    // Verify the conditions to processed with the application when the ok button is pressed
+    // Create a new user when everything is correct
+    @FXML public void handleOkButtonAction(ActionEvent event) throws IOException {
         if (firstNameField.getCharacters().toString().isEmpty() || lastNameField.getCharacters().toString().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle(Main.getResourceBundle().getString("warning"));
@@ -28,6 +35,17 @@ public class AddUserController {
 
             alert.showAndWait();
         }
+    }
 
+    @FXML public void handleCancelButtonAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+
+        stage=(Stage) cancelBtn.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"), Main.getResourceBundle());
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
