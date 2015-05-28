9e53f746dd2d1d2faa906e6bc1c3b57e427c1299
marciodscampos <marciodscampos@gmail.com>
2015-05-28 01:17:29 -0300

    Cadastro usuário completo

--- src/gui/AddUserController.java
+++ src/gui/AddUserController.java
@@ -5,14 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.SplitMenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-import library.users.Student;
-import library.users.User;
-import library.users.UsersFile;
+import library.users.*;
 
 import java.io.IOException;
 
@@ -27,20 +22,32 @@ public class AddUserController {
     @FXML private TextField lastNameField;
     @FXML private TextField emailField;
     @FXML private TextField userIdField;
+    @FXML private MenuItem studentMenu;
+    @FXML private MenuItem teacherMenu;
+    @FXML private MenuItem communityMenu;
+
 
     @FXML public void handleOkButtonAction(ActionEvent event) throws IOException {
         // Cria um objeto de manipulação de dados
         UsersFile file = new UsersFile("UsersReg.txt");
 
         // Verifica se todos os campos já estão preenchidos
-        if (firstNameField.getText().isEmpty() || lastNameField.getText().isEmpty()
-                || emailField.getText().isEmpty() || userIdField.getText().isEmpty()) {
+        if (firstNameField.getText().isEmpty() || lastNameField.getText().isEmpty() || emailField.getText().isEmpty() || userIdField.getText().isEmpty()) {
             // Exibe uma mensagem de erro indicando que os campos não foram preenchidos
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle(Main.getResourceBundle().getString("warning"));
             alert.setHeaderText(null);
             alert.setContentText(Main.getResourceBundle().getString("blankFieldWarning"));
+            alert.showAndWait();
+        }
 
+        // Verifica se o tipo de usuário foi escolhido
+        else if (userType.getText().compareTo(Main.getResourceBundle().getString("group")) == 0) {
+            // Exibe uma mensagem de erro indicando que os campos não foram preenchidos
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle(Main.getResourceBundle().getString("warning"));
+            alert.setHeaderText(null);
+            alert.setContentText(Main.getResourceBundle().getString("blankFieldWarning"));
             alert.showAndWait();
         }
 
@@ -51,7 +58,6 @@ public class AddUserController {
             alert.setTitle(Main.getResourceBundle().getString("warning"));
             alert.setHeaderText(Main.getResourceBundle().getString("invalidEntry"));
             alert.setContentText(Main.getResourceBundle().getString("onlyNumber"));
-
             alert.showAndWait();
         }
 
@@ -62,14 +68,21 @@ public class AddUserController {
             alert.setTitle(Main.getResourceBundle().getString("error"));
             alert.setHeaderText(Main.getResourceBundle().getString("invalidEntry"));
             alert.setContentText(Main.getResourceBundle().getString("userAlreadyRegistered"));
-
             alert.showAndWait();
         }
 
         // Cria o novo usuário e adiciona ao arquivo
         else {
+            String type = userType.getText();
+            User user;
             // Cria um objeto do tipo usuário
-            User user = new Student(firstNameField.getText(), lastNameField.getText(), emailField.getText(), Long.parseLong(userIdField.getText()));
+            if (type.compareTo(Main.getResourceBundle().getString("student")) == 0)
+                user = new Student(firstNameField.getText(), lastNameField.getText(), emailField.getText(), Long.parseLong(userIdField.getText()));
+            else if (type.compareTo(Main.getResourceBundle().getString("teacher")) == 0)
+                user = new Teacher(firstNameField.getText(), lastNameField.getText(), emailField.getText(), Long.parseLong(userIdField.getText()));
+            else
+                user = new CommunityMember(firstNameField.getText(), lastNameField.getText(), emailField.getText(), Long.parseLong(userIdField.getText()));
+
             // Salva o usuário
             file.storeUser(user);
 
@@ -81,9 +94,8 @@ public class AddUserController {
             alert.showAndWait();
 
             // Volta ao menu inicial
-            Stage stage=(Stage) cancelBtn.getScene().getWindow();
+            Stage stage=(Stage) okBtn.getScene().getWindow();
             Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"), Main.getResourceBundle());
-
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
@@ -98,6 +110,15 @@ public class AddUserController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML public void handleSplitMenu(ActionEvent event) {
+        if (event.getSource() == studentMenu)
+            userType.setText(studentMenu.getText());
+        else if (event.getSource() == teacherMenu)
+            userType.setText(teacherMenu.getText());
+        else
+            userType.setText(communityMenu.getText());
     }
 
     private boolean isValidNumber(TextField field) {
